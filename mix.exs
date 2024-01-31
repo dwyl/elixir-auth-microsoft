75ae7eade6521233d60e75e0451cfe79e591ffd2
nelsonic <nelson+github@dwyl.com>
2024-01-31 14:48:36 +0000

    bump version to 1.2.0 to maximise compatibility for httpoison & jason #45

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirAuthMicrosoft.MixProject do
   def project do
     [
       app: :elixir_auth_microsoft,
-      version: "1.1.1",
+      version: "1.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
